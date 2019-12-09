bdbd09dbe02251ba448fb50b58971023af06c7d4
rolandvaszi <rolandvaszi@gmail.com>
2019-12-10 00:59:43 +0200

    Navigation fragment added with four navigation points.

--- app/src/main/java/com/example/moviedb/MainFragment.java
+++ app/src/main/java/com/example/moviedb/MainFragment.java
@@ -3,11 +3,16 @@ package com.example.moviedb;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 /**
@@ -15,6 +20,7 @@ import android.view.ViewGroup;
  */
 public class MainFragment extends Fragment {
 
+    BottomNavigationView bottomNav;
 
     public MainFragment() {
         // Required empty public constructor
@@ -25,7 +31,34 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_main, container, false);
+        bottomNav = (BottomNavigationView) v.findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getFragmentManager().beginTransaction().replace(R.id.navi_fragment_container, new HomeFragment()).commit();
         return v;
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_favorite:
+                            selectedFragment = new FavoritesFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                        case R.id.nav_now:
+                            selectedFragment = new NowPlayingFragment();
+                            break;
+                    }
+                    getFragmentManager().beginTransaction().replace(R.id.navi_fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
 
 }
